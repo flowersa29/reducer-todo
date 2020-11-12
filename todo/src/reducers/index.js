d7290b645a2927cd8b9949008c69c2581a1bf1f2
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-11-12 14:23:03 -0600

    11/12
    added reducer, and state.todolist. code broke.

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,6 @@
+export const initialState ={
+  todoList:[{ text: "move the trash" }, { text: "wash the car" }, { text: "kick the cat" }]
+}
+ export const todoReducer = (state, action) =>{
+    return state
+ }
